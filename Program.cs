6612128dc577b45273f3d6e1a0fbccb0a1be2ce3
Andrew Mayer <amayer42@gmail.com>
2020-04-08 15:40:24 -0400

    add stat manager add roles every hit track attack role stat

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace PIMonsterMash
     {
         static void Main(string[] args)
         {
+            StatsManager statsManager;
+
             Console.SetWindowSize(80, 25);
             Console.BufferWidth = 80;
             Console.BufferHeight = 25;            
@@ -28,18 +30,26 @@ namespace PIMonsterMash
 
             Console.Clear();
             Console.WriteLine("Loading Game, Please Wait...");
-            SetupPIPoints(playerName, serverName);
+
+            statsManager = new StatsManager(playerName, serverName);
+
+            statsManager.IntializePlayerStats();
 
             var terminationKey = new ConsoleKeyInfo('x', ConsoleKey.X, false, false, false);
             var currentKey = new ConsoleKeyInfo();
 
             Console.WriteLine("Press H to attack, X to Leave the game!");
 
+
+
             while (!(currentKey = Console.ReadKey()).Equals(terminationKey))
             {
+                var attackRoll = DiceBag.RollD20();
+                statsManager.UpdateAttackRollStat(attackRoll);
+
                 // Clear Screen
                 Console.Clear();
-                DrawUI();                
+                DrawUI();
             }
         }
 
@@ -50,33 +60,6 @@ namespace PIMonsterMash
             Utils.AlignText("Welcome to The PI Monster Mash!!!", Utils.LineLocation.Center);
             Utils.AlignText("Monster Name", Utils.LineLocation.Center, 50, 50, ConsoleColor.Red);
             Utils.AlignText("Player Name", Utils.LineLocation.BottomRight, 25, 25, ConsoleColor.Green);
-        }
-
-        static void SetupPIPoints(string playerName, string serverName)
-        {
-            // Setup PI Tags
-            var currentPISystem = PISystem.CreatePISystem(serverName, true);
-            var currentPIServer = PIServer.FindPIServer(currentPISystem, serverName);
-
-            // PlayerName - Assuming full name/Unique name
-            // Try to create if not exist
-            var points = PIPoint.FindPIPoints(currentPIServer, playerName + "*");            
-
-            if (points.Count() < 1)
-            {
-                IDictionary<string, object> intAttributeProperties = new Dictionary<string, object> {
-                    { PICommonPointAttributes.PointType, PIPointType.Int32 },
-                    { PICommonPointAttributes.Compressing, 0 },
-                    { PICommonPointAttributes.Shutdown, 0 }
-                };
-
-                IDictionary<string, IDictionary<string, object>> pointsAttributesTable = new Dictionary<string, IDictionary<string, object>>();
-                pointsAttributesTable.Add(playerName + "Score", intAttributeProperties);
-                pointsAttributesTable.Add(playerName + "Rolls", intAttributeProperties);
-                pointsAttributesTable.Add(playerName + "Turns", intAttributeProperties);
-
-                currentPIServer.CreatePIPoints(pointsAttributesTable);
-            }
         }
     }
 }
---
+++ StatsManager.cs
@@ -0,0 +1,82 @@
+﻿using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.PI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMonsterMash
+{
+    class StatsManager
+    {
+        public StatsManager(string playerName, string serverName)
+        {
+            this.playerName = playerName;
+
+            var currentPISystem = PISystem.CreatePISystem(serverName, true);
+            server = PIServer.FindPIServer(currentPISystem, serverName);
+
+            server.Connect();
+        }
+
+        private string playerName;
+
+        private PIServer server;
+
+        private string scoreTagName {
+            get { return $"{playerName}-score"; }
+        }
+
+        private string attackRollsTagName {
+            get { return $"{playerName}-attack-rolls"; }
+        }
+
+        private string damageRollsTagName {
+            get { return $"{playerName}-damage-rolls"; }
+        }
+
+        public void IntializePlayerStats()
+        {
+            // PlayerName - Assuming full name/Unique name
+            // Try to create if not exist
+            var points = PIPoint.FindPIPoints(server, playerName + "*");
+
+            if (points.Count() < 1)
+            {
+                IDictionary<string, object> intAttributeProperties = new Dictionary<string, object> {
+                    { PICommonPointAttributes.PointType, PIPointType.Int32 },
+                    { PICommonPointAttributes.Compressing, 0 },
+                    { PICommonPointAttributes.Shutdown, 0 }
+                };
+
+                IDictionary<string, IDictionary<string, object>> pointsAttributesTable = new Dictionary<string, IDictionary<string, object>>();
+                pointsAttributesTable.Add(scoreTagName, intAttributeProperties);
+                pointsAttributesTable.Add(attackRollsTagName, intAttributeProperties);
+                pointsAttributesTable.Add(damageRollsTagName, intAttributeProperties);
+
+                server.CreatePIPoints(pointsAttributesTable);
+            }
+        }
+
+        public void UpdateScoreStat(int score)
+        {
+            var stat = PIPoint.FindPIPoint(server, scoreTagName);
+            stat.UpdateValue(new AFValue() { Value = score, PIPoint = stat }, AFUpdateOption.InsertNoCompression);
+        }
+
+        public void UpdateAttackRollStat(int attackRoll)
+        {
+            var stat = PIPoint.FindPIPoint(server, attackRollsTagName);
+            stat.UpdateValue(new AFValue() { Value = attackRoll, PIPoint = stat }, AFUpdateOption.InsertNoCompression);
+        }
+
+        public void UpdateDamageRollStat(int damageRoll)
+        {
+            var stat = PIPoint.FindPIPoint(server, damageRollsTagName);
+            stat.UpdateValue(new AFValue() { Value = damageRoll, PIPoint = stat }, AFUpdateOption.InsertNoCompression);
+        }
+    }
+}
