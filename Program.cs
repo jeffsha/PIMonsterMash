4bcaeac3311c2401d3c01d829cd8a5467f93d56b
Jim Johnson <jmjohnson@osisoft.com>
2020-04-08 11:46:01 -0400

    Encapsulate text alignment methods

--- Program.cs
+++ Program.cs
@@ -7,62 +7,29 @@ using OSIsoft.AF;
 using OSIsoft.AF.Asset;
 using OSIsoft.AF.Data;
 using OSIsoft.AF.PI;
+using PIMonsterMash;
 
 namespace PIMonsterMash
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.SetWindowSize(80, 25);
             Console.BufferWidth = 80;
             Console.BufferHeight = 25;
-            CenterText("Welcome to The PI Monster Mash!!!");
             DrawUI();
             Console.ReadLine();
         }
 
-        /// <summary>
-        /// HP of Monster top center
-        ///        Monster
-        ///        Hp of player bottom right
-        /// Instructions: SOME TEXT
-        /// TODO: Move to a utils file
-        /// </summary>
         public static void DrawUI()
         {
             // clear screen, draw basic text ui
-            //Console.Clear();
-            CenterText("Monster 1", 50, 50, ConsoleColor.Red);
-            BottomRightText("Player Name", 25, 25, ConsoleColor.Green);
-        }
-
-        public static void BottomRightText(string s, int hpCurrent = -1, int hpMax = -1, ConsoleColor textColor = ConsoleColor.White)
-        {
-            Console.ForegroundColor = textColor;
-            if (hpCurrent != -1 && hpMax != -1)
-            {
-                s = $"{s} HP: {hpCurrent}/{hpMax}";
-            }
-
-            Console.SetCursorPosition((Console.WindowWidth - s.Trim().Length), Console.BufferHeight - 5);
-            Console.WriteLine(s);
-            Console.ResetColor();
-        }
-
-        public static void CenterText(string s, int hpCurrent = -1, int hpMax = -1, ConsoleColor textColor = ConsoleColor.White)
-        {
-            Console.ForegroundColor = textColor;
-            Console.WriteLine();
-
-            if (hpCurrent != -1 && hpMax != -1)
-            {
-                s = $"{s} HP: {hpCurrent}/{hpMax}";
-            }
-
-            Console.SetCursorPosition((Console.WindowWidth - s.Trim().Length) / 2, Console.CursorTop);
-            Console.WriteLine(s.Trim());
-            Console.ResetColor();
+            Console.Clear();
+            Utils.AlignText("Welcome to The PI Monster Mash!!!", Utils.LineLocation.Center);
+            Utils.AlignText("Monster Name", Utils.LineLocation.Center, 50, 50, ConsoleColor.Red);
+            Utils.AlignText("Player Name", Utils.LineLocation.BottomRight, 25, 25, ConsoleColor.Green);
         }
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMonsterMash
+{
+    static class Utils
+    {
+        public enum LineLocation
+        {
+            Center,
+            BottomRight
+        }
+
+        public static void AlignText(string s, LineLocation lineLocation, int hpCurrent = -1, int hpMax = -1, ConsoleColor textColor = ConsoleColor.White)
+        {
+            Console.ForegroundColor = textColor;
+            Console.WriteLine();
+            s = (hpCurrent != -1 && hpMax != -1) ? $"{s} HP: {hpCurrent}/{hpMax}" : s;
+
+            int cursorLeft = (Console.WindowWidth - s.Trim().Length);
+            int cursorTop = 0;
+            switch (lineLocation)
+            {
+                case LineLocation.BottomRight:
+                    cursorTop = Console.BufferHeight - 5;
+                    break;
+                case LineLocation.Center:
+                    cursorLeft /= 2;
+                    cursorTop = Console.CursorTop;
+                    break;
+                default:
+                    break;
+            }
+            Console.SetCursorPosition(cursorLeft, cursorTop);
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+    }
+}
