7fb708684ead933902f046a9559ab72a07799f7f
Andrew Mayer <amayer@osisoft.com>
2020-04-08 11:26:52 -0400

    add dice bag api and examples

---
+++ DiceBag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMonsterMash
+{
+    static class DiceBag
+    {
+        private static Random rand = new Random();
+
+        public static int RollD4()
+        {
+            return rand.Next(1, 4);
+        }
+
+        public static int RollD6()
+        {
+            return rand.Next(1, 6);
+        }
+
+        public static int RollD8()
+        {
+            return rand.Next(1, 8);
+        }
+
+        public static int RollD10()
+        {
+            return rand.Next(1, 10);
+        }
+
+        public static int RollD12()
+        {
+            return rand.Next(1, 12);
+        }
+
+        public static int RollD20()
+        {
+            return rand.Next(1, 20);
+        }
+
+        public static int RollD100()
+        {
+            return rand.Next(1, 100);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,8 +14,21 @@ namespace PIMonsterMash
     {
         static void Main(string[] args)
         {
-            Console.Write("Welcome to The PI Monster Mash!!!");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to The PI Monster Mash!!!\n");
+
+            //Console.WriteLine();
+            Console.WriteLine("Ye find ye-self in yon Security Guild\n");
+
+            Console.WriteLine("Thy Dice Bag API\n");
+
+            Console.WriteLine("Roll to hit!!!");
+            Console.WriteLine(DiceBag.RollD20());
+
+            Console.WriteLine("Roll for damage!!!");
+            Console.WriteLine(DiceBag.RollD6());
+
+            Console.WriteLine("Roll for your chance to succeed on _____!!!");
+            Console.WriteLine(DiceBag.RollD100());
         }
     }
 }
