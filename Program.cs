148450085ea02cf0c97d94eabe2582dd4ece049e
osi\jshaver <jshaver@osisoft.com>
2020-04-08 10:09:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.PI;
+
+namespace PIMonsterMash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to The PI Monster Mash!!!");
+            Console.ReadLine();
+        }
+    }
+}
